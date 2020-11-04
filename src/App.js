29cbc3f69d0649166a6cee49d4d8b746f8b7eafa
Ramon Rangel Lima <ramon.lima@radixeng.com.br>
2020-11-03 23:25:25 -0300

    aplicando conceito de estado e imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 
 function App() {
+    const [projects, setProjects] =  useState(["Guia Pauta", "Site Crialab"]);
+
+    function handleAddProject() {
+        setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+    }
+
+
     return (
         <>
          <Header title="Projects">
              <ul>
-                 <li>Home</li>
-                 <li>Loja</li>
-                 <li>Produto</li>
+                {projects.map(project => <li key={project}>{project}</li>)}
              </ul>
          </Header>
-         <Header title= "Homepage">
-            <ul>
-                <li>tESTE</li>
-                <li>aBRAMOV</li>
-                <li>GraphQl</li>
-            </ul>
-         </Header>
+
+         <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
         </>
     );
 }

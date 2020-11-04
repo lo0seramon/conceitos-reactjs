5982f2faf9b045545388d0228a5c1dc8a9f22b44
Ramon Rangel Lima <ramon.lima@radixeng.com.br>
2020-11-03 23:57:19 -0300

    Adicionando estilos e imagens

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 
+import backgroundImage from './assets/floyd.jpg';
+
+import './App.css';
+
 function App() {
     const [projects, setProjects] =  useState(["Guia Pauta", "Site Crialab"]);
 
@@ -11,6 +15,7 @@ function App() {
 
     return (
         <>
+         <img width={300} src={backgroundImage} alt="George Floyd"/>
          <Header title="Projects">
              <ul>
                 {projects.map(project => <li key={project}>{project}</li>)}
